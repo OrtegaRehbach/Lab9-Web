9f05f8ecebf451b33609a59fbdda6cac15be9c62
Alejandro Ortega <ort18248@uvg.edu.gt>
2023-05-30 11:08:36 -0600

    Update index.js
    React new version migration

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 require("file-loader?name=[name].[ext]!./index.html");
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "./App.scss";
 
 const appElement = document.getElementById("app");
+const root = createRoot(appElement);
 
-ReactDOM.render(<App />, appElement);
+root.render(<App />);
