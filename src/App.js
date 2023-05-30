96b75d73dff20cd045dbdaf5f7ee8993a2d8d5c7
Alejandro Ortega <ort18248@uvg.edu.gt>
2023-05-30 13:09:37 -0600

    Option checkboxes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Calculator } from "./components/Calculator/Calculator";
 
 export function App() {
+
+    const [allowNegatives, setAllowNegatives] = useState(false);
+    const [percntAsMod, setPercntAsMod] = useState(true);
+
     return (
         <div>
             <h1>Calculadora</h1>
-            <Calculator percntAsMod={true} allowNegatives={false}></Calculator>
+            <Calculator percntAsMod={percntAsMod} allowNegatives={allowNegatives}></Calculator>
+            <div className="checkbox-container">
+                <label>
+                    <input type="checkbox" id="allow-negatives-checkbox" onChange={e => setAllowNegatives(e.target.checked)}/>
+                    Allow Negatives
+                </label>
+                <label>
+                    <input type="checkbox" id="percnt-as-mod-checkbox" defaultChecked={true} onChange={e => setPercntAsMod(e.target.checked)}/>
+                    Percent as Module
+                </label>
+            </div>
         </div>
     );
 }
